78cef9e380a172cf0863cd81ab79a2db81d09d75
Jason <git-admin@casjaysdev.com>
2022-07-03 13:59:09 -0400

    🗃️ modified: etc/init.ps1 🗃️

--- etc/init.ps1
+++ etc/init.ps1
@@ -2,7 +2,6 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
 Install-Module -Name PowerShellGet -AllowPrerelease -Force
 Install-Module -Name PackageManagement -Force
-oh-my-posh init pwsh --config ~/.config/powershell/theme.json | Invoke-Expression
 Install-Module -Name posh-git -Force
 Install-Module -Name posh-sshell -Force
 Install-Module -Name Posh-ACME -Force
