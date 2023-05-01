d838069d94f1b6a7ffd020f8873f2b83904b27a6
casjay <git-admin@casjaysdev.com>
2023-05-01 18:23:55 -0400

    🗃️ Committing everything that changed 🗃️
    bin/setup_powershell
    etc/Microsoft.PowerShell_profile.ps1
    install.sh

--- etc/Microsoft.PowerShell_profile.ps1
+++ etc/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,79 @@
 #!/usr/bin/env pwsh
 Clear-Host
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import modules
+oh-my-posh --init --shell pwsh --config ~/.config/powershell/theme.json | Invoke-Expression
+oh-my-posh completion powershell | Out-String | Invoke-Expression
+Import-Module posh-git
+Import-Module posh-sshell
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+$env:POSH_GIT_ENABLED = $true
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set dracula theme
+Set-PSReadlineOption -Color @{
+    "Command" = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Magenta
+    "Variable" = [ConsoleColor]::White
+    "String" = [ConsoleColor]::Cyan
+    "Number" = [ConsoleColor]::Blue
+    "Type" = [ConsoleColor]::Cyan
+    "Comment" = [ConsoleColor]::DarkCyan
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Autocomplete
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+  $Local:word = $wordToComplete.Replace('"', '""')
+  $Local:ast = $commandAst.ToString().Replace('"', '""')
+  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# posh-git settings
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Dracula Git Status Configuration
+$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function exit { exit }
+function q    { exit }
+function q!   { exit }
+function c    { Clear-Host }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ls
+function ls { /usr/bin/ls -h --color=auto $args }
+function la { /usr/bin/ls -hA --color=auto $args }
+function ll { /usr/bin/ls -hl --color=auto $args }
+function dir { /usr/bin/ls -hD --color=auto $args }
+function lla { /usr/bin/ls -hlA --color=auto $args }
+function l. { /usr/bin/ls -hA $args | /usr/bin/grep -E --color=auto '^\.*'  }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Useful shortcuts for traversing directories
+function  cd..  {  Set-Location ..\ }
 function cd...  { Set-Location ..\.. }
 function cd.... { Set-Location ..\..\.. }
+function  ../    {  Set-Location ..\ }
+function  ...   {  Set-Location ..\..\ }
+function  ....  {  Set-Location ..\..\..\ }
+function  ..... {  Set-Location ..\..\..\..\ }
+function __cmd_exists { /usr/local/bin/cmd_exists --silent $args }
 
 # Compute file hashes - useful for checking successful downloads 
 function md5    { Get-FileHash -Algorithm MD5 $args }
 function sha1   { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
-# Quick shortcut to start notepad
+# Quick shortcut to start editor
 function editor      { myeditor $args }
 
 # Make it easy to edit this profile once it's installed
@@ -28,34 +91,32 @@ function Edit-Profile
 
 Function Test-CommandExists
 {
- Param ($command)
- $oldPreference = $ErrorActionPreference
- $ErrorActionPreference = 'SilentlyContinue'
- try {if(Get-Command $command){RETURN $true}}
- Catch {Write-Host "$command does not exist"; RETURN $false}
- Finally {$ErrorActionPreference=$oldPreference}
+  Param ($command)
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = 'SilentlyContinue'
+  try {if(Get-Command $command){RETURN $true}}
+  Catch {Write-Host "$command does not exist"; RETURN $false}
+  Finally {$ErrorActionPreference=$oldPreference}
 } 
 
-if (Test-CommandExists nvim) {
-    $EDITOR='nvim'
-} elseif (Test-CommandExists pvim) {
-    $EDITOR='pvim'
+if (Test-CommandExists myeditor) {
+        $EDITOR='myeditor'
 } elseif (Test-CommandExists vim) {
     $EDITOR='vim'
+} elseif (Test-CommandExists pvim) {
+    $EDITOR='pvim'
+} elseif (Test-CommandExists pvim) {
+    $EDITOR='pvim'
 } elseif (Test-CommandExists vi) {
     $EDITOR='vi'
 } elseif (Test-CommandExists code) {
-    #VS Code
     $EDITOR='code'
 } elseif (Test-CommandExists notepad) {
     #fallback to notepad since it exists on every windows machine
     $EDITOR='notepad'
 }
-Set-Alias -Name vim -Value $EDITOR
 
-
-function ll { Get-ChildItem -Path $pwd -File }
-function g { Set-Location $HOME\Documents\Github }
+function g { Set-Location $HOME\Projecys\github }
 function gcom
 {
 	git add .
@@ -113,49 +174,5 @@ function pkill($name) {
 function pgrep($name) {
         Get-Process $name
 }
-
-# Import modules
-oh-my-posh --init --shell pwsh --config ~/.config/powershell/theme.json | Invoke-Expression
-oh-my-posh completion powershell | Out-String | Invoke-Expression
-Import-Module posh-git
-Import-Module posh-sshell
-
-# set ENV
-$env:POSH_GIT_ENABLED = $true
-
-# Set dracula theme
-Set-PSReadlineOption -Color @{
-    "Command" = [ConsoleColor]::Green
-    "Parameter" = [ConsoleColor]::Gray
-    "Operator" = [ConsoleColor]::Magenta
-    "Variable" = [ConsoleColor]::White
-    "String" = [ConsoleColor]::Cyan
-    "Number" = [ConsoleColor]::Blue
-    "Type" = [ConsoleColor]::Cyan
-    "Comment" = [ConsoleColor]::DarkCyan
-}
-
-# Autocomplete
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
-  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-  $Local:word = $wordToComplete.Replace('"', '""')
-  $Local:ast = $commandAst.ToString().Replace('"', '""')
-  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-  }
-}
-
-# posh-git settings
-$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
-$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
-$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
-$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
-$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
-
-# Dracula Git Status Configuration
-$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
 
 "You are now entering PowerShell : " + $env:USER
